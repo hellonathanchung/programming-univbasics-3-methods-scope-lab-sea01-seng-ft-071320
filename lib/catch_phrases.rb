ca9971318535946d8692e71d399287a8ca0b98d4
Nathan Chung <chung.nathanh@gmail.com>
2020-06-12 20:41:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
-def mario do
+describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
-    phrase = "It's a me, Mario!"
-    expect{mario}.to output("It's-a me, Mario!.\n").to_stdout
+    phrase = "It's-a me, Mario!"
+    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
 
